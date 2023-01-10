49417f50ffc76a434cd5b32e73f31e10a4e3961a
Victor Marcas <victor11687@gmail.com>
2023-01-10 01:18:01 -0500

    feat: add first atom componente and config eslint

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup>
+import Button from '@/components/atoms/Button.vue'
+</script>
 <template>
-  <div class="w-full h-screen bg-gray-100"></div>
+  <div
+    class="flex items-center justify-center w-full h-screen grid-cols-1 bg-gray-100"
+  >
+    <Button type="button">Button Primary</Button>
+  </div>
 </template>
---
+++ components/atoms/Button.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'button',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['click'])
+const onClick = () => emit('click')
+</script>
+
+<template>
+  <button
+    :type="type"
+    :disabled="disabled"
+    @click="onClick"
+    class="inline-flex items-center justify-center gap-2 px-4 py-2 text-sm font-semibold text-white transition-all duration-200 transform scale-105 bg-purple-500 rounded-md hover:bg-purple-500/90 disabled:bg-gray-300 disabled:opacity-90 disabled:cursor-default"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+}
+</script>
