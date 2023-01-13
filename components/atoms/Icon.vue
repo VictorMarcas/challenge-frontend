71604a435909e90d81a4b6714d8857514896c6c0
Victor Marcas <victor11687@gmail.com>
2023-01-12 20:05:17 -0500

    feat: Se ha agregado icons en svg y su componente

---
+++ components/atoms/Icon.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <nuxt-icon
+    :name="name"
+    filled
+    class="inline-flex items-center justify-center [&>svg]:!w-full [&>svg]:!h-full [&>svg]:!m-0"
+  />
+</template>
