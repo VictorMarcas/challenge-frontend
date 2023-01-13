d75bc327f51d21d7f0407faa8a1b8796642e6ca5
Victor Marcas <victor11687@gmail.com>
2023-01-12 20:06:08 -0500

    feat: se ha agregado configuración de svgr en vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,17 @@ import { tailwindConfig } from './config/tailwindcss'
 import { googleFontsConfig } from './config/google-fonts'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/google-fonts', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+    'nuxt-icons',
+    '@nuxt/image-edge',
+  ],
   tailwindcss: tailwindConfig,
   googleFonts: googleFontsConfig,
+  runtimeConfig: {
+    public: {
+      api_baseURL: process.env.API_BASE_URL,
+    },
+  },
 })
