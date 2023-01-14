0ad07da2db39290edb2ef88ba3b26790c28f5d0c
Victor Marcas <victor11687@gmail.com>
2023-01-14 11:05:34 -0500

    feat: se ha agregado usuarios en index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <script setup>
+import { onMounted } from 'vue'
+import { useUsers } from '@/composables/useUsers'
 import UserCapsule from '@/components/molecules/UserCapsule.vue'
+import UserCapsuleSkeleton from '@/components/molecules/UserCapsuleSkeleton.vue'
+const { users, getUsers } = useUsers()
+onMounted(() => {
+  getUsers()
+})
 </script>
 <template>
-  <div class="flex flex-col items-center justify-center h-full py-2">
-    <div class="max-w-sm">
-      <UserCapsule />
+  <section class="py-12">
+    <div
+      class="container grid grid-cols-1 gap-4 md:gap-6 md:grid-cols-2 xl:grid-cols-3"
+    >
+      <template v-if="users.length === 0">
+        <UserCapsuleSkeleton v-for="(_, index) in 8" :key="index" />
+      </template>
+      <template v-else>
+        <UserCapsule v-for="user in users" :key="user.id" :user="user" />
+      </template>
     </div>
-  </div>
+  </section>
 </template>
