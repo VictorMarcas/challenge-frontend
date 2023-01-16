e25256927f13f1ceccfff2f7b2f10e1e6ef5a959
Victor Marcas <victor11687@gmail.com>
2023-01-15 22:21:51 -0500

    chore: se ha agregado configuracion de head

---
+++ config/head.ts
@@ -0,0 +1,19 @@
+export const head = {
+  title: 'Frontend Challenge Talently',
+  htmlAttrs: {
+    lang: 'es',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Frontend Challenge Talently',
+    },
+  ],
+  link: [],
+  style: [],
+  script: [],
+  noscript: [],
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 import { defineNuxtConfig } from 'nuxt/config'
 import { tailwindConfig } from './config/tailwindcss'
 import { googleFontsConfig } from './config/google-fonts'
+import { head } from './config/head'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head,
+  },
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss',
