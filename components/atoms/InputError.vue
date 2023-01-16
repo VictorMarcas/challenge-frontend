73224690800fbb1d6cde3d7a3780283cfd31db8f
Victor Marcas <victor11687@gmail.com>
2023-01-15 21:58:31 -0500

    feat: componente para texto de errores en form

---
+++ components/atoms/InputError.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+<template>
+  <span class="block text-sm text-red-500 leading-thign">{{ label }}</span>
+</template>
